b2a50fc1ae07d9e203d6ff633ab294d2a1cad4c3
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2020-05-12 09:55:42 +0200

    menu är fixat med bug
    Menu fungerar inte kanske gör jag fel. går efter banken.py enlingt det har det menu som jag har jobbat som jag följer dess principen.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Min loggbok
- Denna är sista python program som ska skriva  ska föreställa sig som en digital logbok
+ Denna är sista python program som ska skriva  ska föreställa sig som en digital loggbok
---
+++ main.py
@@ -0,0 +1,30 @@
+
+print('Hej wälkommen till ditt loggbok du kommer får välja bland menyer, för att hantera ditt loggbok.')
+print('\v')
+
+menu = 0
+answer1 = 'No, no, n, N'
+answer2 = 'Yes, yes, y, Y'
+
+while menu != 4:
+    print('Välja bland menyerna (1= Visa alla logg, 2= Skapa ny logg, 3= Spara loggfilen, 4= exit programmen')
+    a = int(input('Val: '))
+    print('\v')
+
+    if menu == 1:
+        print('\v')
+        print('Är du säker du har valt? Visa logg? Yes, yes, y, Y')
+        answer = input('Yes or No ?: ')
+    
+    elif menu == 2:
+        print('skapa ny logg')
+    
+    elif menu == 3:
+        print('spara logen')
+    
+    elif menu == 4:
+        print('exit')
+        exit()
+    
+    else:
+        print('Nånting gick fel')
